23db619d05516168655d98394ec7bd992b43ba8b
Rustam <rustam.dranitsyn@mail.ru>
2020-09-18 20:35:46 +0600

    Закончил 7 урок

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
-    $i = 1;
+//     $name["Ivanov"] = "Alex";
+//     $name["Wilsher"]="Jeff";
+//     $name["Spectr"] = "Harvey";
+//     echo $name["Ivanov"];
 
-    while($i <= 10) {
-        echo $i."<br>";
-        $i++;
-    }
+//     define('masiv' ,
+//         ["ivonova" => ["name"=>"ivan", "born"=>"12-03-1990"]]    
+// );
+// echo masiv["ivonova"]["name"];
+// $number = count($name);
+// echo $number;
+
+$birth = [
+    "Thomas"=>"1980-11-18",
+    "John"=>"1992-04-28"
+];
+foreach ($birth as $k=>$v)
+    echo "$k Родился $v <br>";
+
 
 ?>
 
